447d1a30f919bf601d4b8f58c76fdd642e31bca2
miscreationist <606cut@gmail.com>
2025-08-21 19:35:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,49 +2,6 @@ import streamlit as st
 import random
 import re
 
-# --- Map Data: Midwich Elementary School ---
-map_data = {
-    "small_items": [
-        "1d4 pieces of chalk",
-        "Poster [choose: star, sunflower, monkey]",
-        "1d4 stack of music paper",
-        "Pens/Pencils",
-        "1d8 thumb tacks",
-        "1d4 Medkit [+1 to your next trial]",
-        "1d6 Metallic star stickers"
-    ],
-    "medium_items": [
-        "Globe, out of date",
-        "School text books [choose: biology, history, english]",
-        "Projector, requires power",
-        "Desk chair",
-        "Mop and bucket",
-        "Tray",
-        "Duffle bag with 1d4 shirts",
-        "Locked locker. DC 15 to break it open. If you do, roll on locker table",
-        "VHS tapes. Looks to be kids movies",
-        "Wall clock"
-    ],
-    "large_items": [
-        "1d4 + 2 Teachers chairs",
-        "Desk",
-        "Media/Book cart, but it is upstairs, you need help carrying it down the stairs, lest the denizens of the realm hear you",
-        "Bench"
-    ],
-    "penalties_table": [
-        "A creature in the fog has found you and killed you. When you wake back up you have a -2 penalty for 1d12 matches and are unable to scavenge until the negative penalty is gone, or it is the next day",
-        "You were injured and will take a day to recover. There is a -1 penalty for 1d6 matches",
-        "If you were carrying something scavenged earlier, you lose it [but if you had nothing, you lost nothing]",
-        "You were chased until you reached your campfire, but any time you try to scavenge again you feel something following you until you reach your campfire. You are unable to scavenge until the next day"
-    ],
-    "locker_table": [
-        "There is a feral rat that bites you badly enough to injure your hand, no more scavenging today",
-        "A small pair of shoes",
-        "1d2 brand new parts [+1 to your next trial]",
-   import streamlit as st
-import random
-import re
-
 # --- Map Data ---
 maps_data = {
     "Midwich Elementary School": {
@@ -188,4 +145,3 @@ if roll_button:
         for item in loot_obtained:
             if "Locked locker" not in item:
                 st.write(f"- {item}")
-
